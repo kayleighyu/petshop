8e93098af1ee057004c47a103483d44f5f0daaf1
Kayleigh Yu <kayleighyu@gmail.com>
2017-03-10 19:25:18 -0500

    Successfully implemented the DELETE request

--- index.js
+++ index.js
@@ -79,7 +79,21 @@ server.post('/animals', function(req, res){
   });
 });
 //PUT /animals/:id
+
 //DELETE /animals/:id
+server.delete('/animals/:id', function(req, res){
+  Animal.remove({_id: req.params.id}, function(err, documents){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully deleted'
+      });
+    }
+  });
+});
 
 server.listen(port, function(){
   console.log('Now listening on port...', port);
